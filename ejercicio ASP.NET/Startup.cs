604b117ebdad2ccd34826fe5aa879aa3a7cb029c
Jomecorib08 <meliza08@hotmail.es>
2017-03-31 07:37:52 -0500

    Proyecto ejercicios de clase

---
+++ ejercicio ASP.NET/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ejercicio_ASP.NET.Startup))]
+namespace ejercicio_ASP.NET
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
