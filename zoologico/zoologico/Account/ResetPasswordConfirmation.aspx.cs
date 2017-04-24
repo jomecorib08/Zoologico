643250e3d455d7ef2e659e059c83f6675e98d88c
vanesa <vanesa.trujillo@udea.edu.co>
2017-04-24 07:59:13 -0500

    vanesa

--- zoologico/zoologico/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace zoologico.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- zoologico/zoologico/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zoologico.Global" Language="C#" %>
